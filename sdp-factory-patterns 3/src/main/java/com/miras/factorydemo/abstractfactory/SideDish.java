7c91344acf0060537d6af9251622221df16d2f16
MirasSapan <zharkinbekulym@gmail.com>
2026-09-21 14:29:41 +0500

    Update SideDish.java

--- sdp-factory-patterns 3/src/main/java/com/miras/factorydemo/abstractfactory/SideDish.java
+++ sdp-factory-patterns 3/src/main/java/com/miras/factorydemo/abstractfactory/SideDish.java
@@ -1,8 +1,5 @@
 package com.miras.factorydemo.abstractfactory;
 
-/**
- * Abstract Product #2 - one interface per product type in the family.
- */
 public interface SideDish {
 
     String getName();
