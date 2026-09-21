aaac31cd6e86d5e8b5315b92a5d495f2b7ecc8aa
MirasSapan <zharkinbekulym@gmail.com>
2026-09-21 14:00:03 +0500

    Update MainDish.java

--- sdp-factory-patterns 3/src/main/java/com/miras/factorydemo/abstractfactory/MainDish.java
+++ sdp-factory-patterns 3/src/main/java/com/miras/factorydemo/abstractfactory/MainDish.java
@@ -1,8 +1,6 @@
 package com.miras.factorydemo.abstractfactory;
 
-/**
- * Abstract Product #1 - one interface per product type in the family.
- */
+
 public interface MainDish {
 
     String getName();
